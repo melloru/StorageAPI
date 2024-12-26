ad3d5e9c0cf1959e4c60fe597f2ea2ffaf708d9f
melloru <pythonubuntu12@gmail.com>
2024-12-25 22:08:33 -0200

    Добавил: - Абстрактрный класс моделей Base. - Аннотации полей для удобства создания таблиц.

--- app/database.py
+++ app/database.py
@@ -1,4 +1,9 @@
+from datetime import datetime
+from typing import Annotated
+
+from sqlalchemy import func
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from sqlalchemy.orm import mapped_column, Mapped, DeclarativeBase
 
 from app.config import get_db_url
 
@@ -6,4 +11,16 @@ DATABASE_URL = get_db_url()
 engine = create_async_engine(DATABASE_URL, echo=True)
 async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
 
+int_pk = Annotated[int, mapped_column(primary_key=True)]
+created_at = Annotated[datetime, mapped_column(server_default=func.now())]
+updated_at = Annotated[datetime, mapped_column(server_default=func.now(), onupdate=datetime.now)]
+str_uniq = Annotated[str, mapped_column(unique=True, nullable=False)]
+str_null_false = Annotated[str, mapped_column(nullable=False)]
+str_null_true = Annotated[str, mapped_column(nullable=True)]
 
+
+class Base(DeclarativeBase):
+    __abstract__ = True
+
+    created_at: Mapped[created_at]
+    updated_at: Mapped[updated_at]
