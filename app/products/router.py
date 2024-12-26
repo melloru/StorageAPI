bd88c502387486921aaf04b8918cc9412c9af312
melloru <pythonubuntu12@gmail.com>
2024-12-26 18:36:17 -0200

    + '/{id}' get_product_by_id получить товар по ID

--- app/products/router.py
+++ app/products/router.py
@@ -8,3 +8,11 @@ router = APIRouter(prefix='/products', tags=['Работа с товаром'])
 @router.get('/', summary='Получить список всех товаров')
 async def get_all_products() -> list[SProduct]:
     return await ProductDAO.find_all()
+
+
+@router.get('/{id}', summary='Получить товар по ID')
+async def get_product_by_id(product_id: int) -> SProduct | dict:
+    result = await ProductDAO.find_one_or_none(product_id)
+    if result is None:
+        return {'message': f'Товара с ID {product_id} не существует.'}
+    return result
