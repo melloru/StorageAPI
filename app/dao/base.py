b5d4db4c8d229c6dc7ec2f440b7188a7d972cc5f
melloru <pythonubuntu12@gmail.com>
2024-12-26 18:32:10 -0200

    BaseDAO + метод find_one_or_none.

--- app/dao/base.py
+++ app/dao/base.py
@@ -11,3 +11,10 @@ class BaseDAO:
             query = select(cls.model).filter_by(**filter_by)
             result = await session.execute(query)
             return result.scalars().all()
+
+    @classmethod
+    async def find_one_or_none(cls, data_id: int):
+        async with async_session_maker() as session:
+            query = select(cls.model).filter_by(id=data_id)
+            result = await session.execute(query)
+            return result.scalar_one_or_none()
